5b8ebee12f559f310c7b751037064852fc50961c
hidamari1015 <86.sano.masahiro@gmail.com>
2020-11-05 11:49:41 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefectures
+  belongs_to :day
+
+  with_options presence: true do
+    validates :title
+    validates :category_id
+    validates :status_id
+    validates :catch_copy
+    validates :price
+    validates :delivery_fee_id
+    validates :prefectures_id
+    validates :day_id
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+  validates :price, format: { with: /\A[0-9]+\z/}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_092236) do
     t.integer "price", null: false
     t.integer "delivery_fee_id", null: false
     t.integer "prefectures_id", null: false
-    t.integer "days_id", null: false
+    t.integer "day_id", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
