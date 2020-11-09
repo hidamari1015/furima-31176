01115972556bafdfeefa1dc19239c5675de2b286
hidamari1015 <86.sano.masahiro@gmail.com>
2020-11-09 14:30:23 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
-  
+
   def index
     @items = Item.all.order('created_at DESC')
   end
