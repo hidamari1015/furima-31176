a3ec529c4d79a6813d828dc521e7911535627f4d
hidamari1015 <86.sano.masahiro@gmail.com>
2020-11-09 20:26:25 +0900

    削除機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -29,10 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
       @item.destroy
       redirect_to action: :index
-    end
   end
 
   private
